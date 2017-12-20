ee7a47605d155cea51a6a08966e53060f68cb920
glisandro <gustavomartinez@gmail.com>
2017-12-20 12:47:59 -0300

    Agrego bootstrap con scss

--- webpack.config.js
+++ webpack.config.js
@@ -9,13 +9,13 @@ Encore
     .enableSourceMaps(!Encore.isProduction())
     // uncomment to create hashed filenames (e.g. app.abc123.css)
     // .enableVersioning(Encore.isProduction())
-
-    // uncomment to define the assets of the project
-     .addEntry('js/app', './assets/js/app.js')
-    // .addStyleEntry('css/app', './assets/css/app.scss')
-
     // uncomment if you use Sass/SCSS files
-    // .enableSassLoader()
+    .enableSassLoader()
+    // uncomment to define the assets of the project
+    .addEntry('js/app', './assets/js/app.js')
+    .addStyleEntry('css/app', './assets/scss/app.scss')
+
+    
 
     // uncomment for legacy applications that require $/jQuery as a global variable
      .autoProvidejQuery()
